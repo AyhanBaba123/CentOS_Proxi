5b1f8f501e75927c256b7dcadb74e5fac9a5342f
AyhanBaba123 <109274369+AyhanBaba123@users.noreply.github.com>
2022-07-14 11:00:36 +0300

    deneme.sh

--- CentOS_Proxi.sh
+++ CentOS_Proxi.sh
@@ -2,8 +2,8 @@
 # Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
 
 #------------------#
-KULLANICI="tarak"
-SIFRE="kurek"
+KULLANICI="242"
+SIFRE="cagatay"
 #------------------#
 
 #------------------#
